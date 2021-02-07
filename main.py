c4e2da4fcdd31bf4e674e50c7c28e61b9e9f1244
Alick Campbell <alickcampbell54321@gmail.com>
2021-02-06 20:36:59 -0500

    Added all data routes

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pytz import datetime
 import pytz
 
-successDIct = {}
 app = Flask(__name__)
 
+#global dictionary to store information about a single user 
 userData = {}
+
+#global list and counter to store items related to the data route 
+counter = 0
+BUFFER = []
 
 #Profile  Routes 
 @app.route('/profile')
@@ -59,6 +63,44 @@ def profile_patch():
         return redirect(url_for("profile_get"))
 
 #Data Routes 
+@app.route("/data")
+def data_get():
+    return jsonify(BUFFER)
+
+@app.route("/data", methods = ["POST"])
+def data_post():
+    global counter
+    tankTemp = request.json
+    counter+=1
+    tankTemp["id"] = str(counter)
+    BUFFER.append(tankTemp)
+    return jsonify(tankTemp)
+
+@app.route('/data/<int:id>', methods = ["PATCH"])
+def data_patch(id):
+    patch = request.json
+    found = False
+    for dictionaries in BUFFER:
+        if dictionaries["id"] == str(id):
+            found = True 
+            #update BUFFER 
+            dictionaries["location"] = patch["location"]
+            dictionaries["lat"] = patch["lat"]
+            dictionaries["long"] = patch["long"]
+            dictionaries["percentage_full"] = patch["percentage_full"]
+            #no need to go through the rest of items if target found
+            break 
+    if found == False:
+        #show user everything in the list if user tries to update a nonexistent record
+        return redirect(url_for("data_get"))
+    return jsonify(BUFFER[id-1]) #data in the list is indexed as its id - 1
+
+@app.route('/data/<int:id>', methods = ["DELETE"])
+def data_delete(id):
+    BUFFER.remove(BUFFER[id-1])
+    successDict = { "success":True,}
+    return jsonify(successDict)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
